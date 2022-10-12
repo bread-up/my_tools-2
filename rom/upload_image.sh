14a21cba1954ccd450c0c8698d98270168253037
AndVer2 <72114405+AndVer2@users.noreply.github.com>
2022-10-12 22:36:20 +0100

    Update upload_image.sh

--- rom/upload_image.sh
+++ rom/upload_image.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+cd /root/crdroid
 mkdir /root/image
 
-cp out/target/product/m10lte/boot.img /root/image
-cp out/target/product/m10lte/recovery.img /root/image
-cp out/target/product/m10lte/vendor.img /root/image
+ls out/target/product/m10lte
+cd out/target/product/m10lte
+cp boot.img /root/image
+cp recovery.img /root/image
+cp vendor.img /root/image
 
 cd /root/image
 
@@ -12,4 +15,4 @@ zip image.zip *
 
 export OUTPUT="image.zip" && FILENAME=$(echo $OUTPUT) && curl -T $FILENAME https://oshi.at/${FILENAME}/${OUTPUT} > mirror.txt || { echo "WARNING: Failed to Mirror the Build!"; } && MIRROR_LINK=$(cat mirror.txt | grep Download | cut -d\  -f1) && echo $MIRROR_LINK
 
-cd /rom/crdroid
+cd /root/crdroid
