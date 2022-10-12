0dc3691a1b8ffcc2da683c933b35c96b22f34628
AndVer2 <72114405+AndVer2@users.noreply.github.com>
2022-10-12 19:50:46 +0100

    Update sync.sh

--- rom/sync.sh
+++ rom/sync.sh
@@ -1,1 +1,37 @@
+#!/bin/bash
 
+# install package..
+apt update -y
+apt install bc sudo git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc libncurses5 unzip python-is-python3 -y
+# run repo..
+
+mkdir ~/bin
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+chmod a+x ~/bin/repo
+ln -sf ~/bin/repo /usr/bin/repo
+
+# set configs.
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
+
+# Create dirs
+cd /root
+mkdir crdroid ; cd crdroid
+
+# Init repo
+repo init --depth=1 -u https://github.com/crdroidandroid/android.git -b 10.0
+
+# Clone my local repo
+git clone https://github.com/AndVer2/android_manifest_samsung_m10lte.git .repo/local_manifests
+
+# Sync
+repo sync -j`nproc`
+
+# Build
+. build/envsetup.sh
+lunch lineage_m10lte-eng
+
+mka api-stubs-docs
+mka hiddenapi-lists-docs
+mka system-api-stubs-docs
+mka test-api-stubs-docs
