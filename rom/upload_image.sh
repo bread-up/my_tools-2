a627b1db92fc52feb74b726431e1965f7b67d5ba
AndVer2 <72114405+AndVer2@users.noreply.github.com>
2022-10-13 13:22:25 +0100

    Update upload_image.sh

--- rom/upload_image.sh
+++ rom/upload_image.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-cd /root/crdroid
-mkdir /root/image
+#cd /root/crdroid
+#mkdir /root/image
 
-ls out/target/product/m10lte
-cd out/target/product/m10lte
-cp boot.img /root/image
-cp recovery.img /root/image
-cp vendor.img /root/image
+#ls out/target/product/m10lte
+#cd out/target/product/m10lte
+#cp boot.img /root/image
+#cp recovery.img /root/image
+#cp vendor.img /root/image
 
-cd /root/image
+#cd /root/image
 
-zip image.zip *
+#zip image.zip *
 
-export OUTPUT="image.zip" && FILENAME=$(echo $OUTPUT) && curl -T $FILENAME https://oshi.at/${FILENAME}/${OUTPUT} > mirror.txt || { echo "WARNING: Failed to Mirror the Build!"; } && MIRROR_LINK=$(cat mirror.txt | grep Download | cut -d\  -f1) && echo $MIRROR_LINK
+#export OUTPUT="image.zip" && FILENAME=$(echo $OUTPUT) && curl -T $FILENAME https://oshi.at/${FILENAME}/${OUTPUT} > mirror.txt || { echo "WARNING: Failed to Mirror the Build!"; } && MIRROR_LINK=$(cat mirror.txt | grep Download | cut -d\  -f1) && echo $MIRROR_LINK
 
-cd /root/crdroid
+#cd /root/crdroid
