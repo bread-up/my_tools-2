ba54394942093b8a5fcc3ba3d6e8f9c58063a23b
AndVer2 <72114405+AndVer2@users.noreply.github.com>
2022-10-14 17:21:28 +0100

    Delete build_rom.sh

--- rom/build_rom.sh
+++
@@ -1,7 +0,0 @@
-#cd /root/crdroid
-#. build/envsetup.sh
-#lunch lineage_m10lte-eng
-#mka bacon -j`nproc`
-
-#cd out/target/product/m10lte
-#export OUTPUT1="*m10lte*.zip" && FILENAME1=$(echo $OUTPUT1) && curl -T $FILENAME1 https://oshi.at/${FILENAME1}/${OUTPUT1} > mirror.txt || { echo "WARNING: Failed to Mirror the Build!"; } && MIRROR_LINK1=$(cat mirror.txt | grep Download | cut -d\  -f1) && echo $MIRROR_LINK1
