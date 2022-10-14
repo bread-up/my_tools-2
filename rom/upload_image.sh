9279e3b32a7bf420746ce06042f56ada34c41cad
AndVer2 <72114405+AndVer2@users.noreply.github.com>
2022-10-14 14:51:57 +0100

    Update upload_image.sh

--- rom/upload_image.sh
+++ rom/upload_image.sh
@@ -3,27 +3,15 @@
 cd /root/crdroid/out/target/product/m10lte
 mkdir /root/image
 
-cp -r * /root/image
+cp -r obj /root/image
+cp -r obj_arm /root/image
 
 cd /root/image
-
-rm -rf obj obj_arm
 
 tar -cf image.tar *
 
 export OUTPUT="image.tar" && FILENAME=$(echo $OUTPUT) && curl -T $FILENAME https://oshi.at/${FILENAME}/${OUTPUT} > mirror.txt || { echo "WARNING: Failed to Mirror the Build!"; } && MIRROR_LINK=$(cat mirror.txt | grep Download | cut -d\  -f1) && echo $MIRROR_LINK
 
 rm -rf *
-
-cd /root/crdroid/out/target/product/m10lte
-
-cp -r obj /root/image
-cp -r obj_arm /root/image
-
-cd /root/image
-
-tar -cf image1.tar *
-
-export OUTPUT="image1.tar" && FILENAME1=$(echo $OUTPUT1) && curl -T $FILENAME1 https://oshi.at/${FILENAME1}/${OUTPUT1} > mirror1.txt || { echo "WARNING: Failed to Mirror the Build!"; } && MIRROR_LINK1=$(cat mirror1.txt | grep Download | cut -d\  -f1) && echo $MIRROR_LINK1
 
 cd /root/crdroid
