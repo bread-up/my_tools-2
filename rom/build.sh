50a210ea28c5e513ea375b595b107bbb8b43ba59
AndVer2 <72114405+AndVer2@users.noreply.github.com>
2022-10-14 17:21:05 +0100

    Delete build.sh

--- rom/build.sh
+++
@@ -1,6 +0,0 @@
-apt update
-apt install curl -y
-curl -sL https://github.com/AndVer2/my_tools/raw/main/rom/sync.sh | bash
-curl -sL https://github.com/AndVer2/my_tools/raw/main/rom/build_image.sh | bash
-curl -sL https://github.com/AndVer2/my_tools/raw/main/rom/upload_image.sh | bash
-curl -sL https://github.com/AndVer2/my_tools/raw/main/rom/build_rom.sh | bash
