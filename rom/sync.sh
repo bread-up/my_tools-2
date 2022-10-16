5bd647e2c42a5085a8126424cdc95915db0aee0c
AndVer2 <72114405+AndVer2@users.noreply.github.com>
2022-10-16 07:59:27 +0100

    Update sync.sh

--- rom/sync.sh
+++ rom/sync.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# install package..
+# install package
 apt update -y
 apt install sudo git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc libncurses5 unzip python-is-python3 -y
-# run repo..
+# run repo
 
 mkdir ~/bin
 curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
