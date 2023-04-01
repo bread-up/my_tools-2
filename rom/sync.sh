852da7d09729a2190b4bdb0e068091c94e407c2e
bread-up <126802193+bread-up@users.noreply.github.com>
2023-04-01 21:49:17 +0000

    Update sync.sh

--- rom/sync.sh
+++ rom/sync.sh
@@ -28,23 +28,18 @@ cd /root
 mkdir crdroid ; cd crdroid
 
 # Init repo
-repo init --depth=1 -u https://github.com/LineageOS/android -b lineage-17.1
+repo init --depth=1 -u https://github.com/crdroidandroid/android -b 8.1
 
 # Clone my local repo
-git clone https://github.com/AndVer9/android_manifest_samsung_m10lte.git -b lineage-17.1 .repo/local_manifests
+git clone https://github.com/youssefnone/android_manifest_samsung_m10lte.git -b crdroid-10 .repo/local_manifests
 
 # Sync
 repo sync --no-repo-verify -c --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune -j`nproc`
 
 # Build
 . build/envsetup.sh
-lunch lineage_m10lte-userdebug
+lunch lineage_m10lte-eng
 
-mka kernel
-mka api-stubs-docs
-mka hiddenapi-lists-docs
-mka system-api-stubs-docs
-mka test-api-stubs-docs
 mka bacon -j`nproc`
 cd out/target/product/m10lte
 export OUTPUT="*m10lte*zip" && FILENAME=$(echo $OUTPUT) && curl -T $FILENAME https://oshi.at/${FILENAME}/${OUTPUT} > mirror.txt || { echo "WARNING: Failed to Mirror the Build!"; } && MIRROR_LINK=$(cat mirror.txt | grep Download | cut -d\  -f1) && echo $MIRROR_LINK
