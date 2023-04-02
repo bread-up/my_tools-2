398f63a7de8356769aecb19ece2382d85fc3fea1
bread-up <126802193+bread-up@users.noreply.github.com>
2023-04-02 21:55:11 +0000

    Update sync.sh

--- rom/sync.sh
+++ rom/sync.sh
@@ -2,13 +2,22 @@
 
 # install package
 apt update -y
-apt install locales sudo git-core tmate gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc libncurses5 unzip python3 -y
-
+apt install sudo git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc libncurses5 unzip -y
 # run repo
+
 mkdir ~/bin
 curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
 ln -sf ~/bin/repo /usr/bin/repo
+
+curl -L -o h.deb http://mirrors.kernel.org/ubuntu/pool/main/b/bc/bc_1.07.1-2_amd64.deb
+dpkg -i h.deb
+
+curl -L -o l.deb http://mirrors.kernel.org/ubuntu/pool/main/p/popt/libpopt0_1.16-11_amd64.deb
+dpkg -i l.deb
+
+curl -L -o i.deb http://security.ubuntu.com/ubuntu/pool/main/r/rsync/rsync_3.1.2-2.1ubuntu1.5_amd64.deb
+dpkg -i i.deb
 
 # set configs.
 git config --global user.email "you@example.com"
@@ -19,7 +28,7 @@ cd /root
 mkdir crdroid ; cd crdroid
 
 # Init repo
-repo init --depth=1 -u https://github.com/bread-up/android -b 8.1
+repo init --depth=1 -u https://github.com/bread-up/android -b 10.0
 
 # Clone my local repo
 git clone https://github.com/youssefnone/android_manifest_samsung_m10lte.git -b crdroid-10 .repo/local_manifests
@@ -29,8 +38,8 @@ repo sync --no-repo-verify -c --force-sync --no-clone-bundle --no-tags --optimiz
 
 # Build
 . build/envsetup.sh
-lunch lineage_m10lte-user
-mka bacon -j$(nproc --all)
+lunch lineage_m10lte-userdebug
 
+mka bacon -j$(nproc --all)
 cd out/target/product/m10lte
 export OUTPUT="*m10lte*zip" && FILENAME=$(echo $OUTPUT) && curl -T $FILENAME https://oshi.at/${FILENAME}/${OUTPUT} > mirror.txt || { echo "WARNING: Failed to Mirror the Build!"; } && MIRROR_LINK=$(cat mirror.txt | grep Download | cut -d\  -f1) && echo $MIRROR_LINK
