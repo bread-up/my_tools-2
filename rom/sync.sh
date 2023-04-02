06b6b50feadc2d7b3b64f03f31f187c0519022bf
bread-up <126802193+bread-up@users.noreply.github.com>
2023-04-02 16:21:14 +0000

    Update sync.sh

--- rom/sync.sh
+++ rom/sync.sh
@@ -2,8 +2,10 @@
 
 # install package
 apt update -y
-apt install sudo git-core tmate gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc libncurses5 unzip python3 -y
+apt install locales sudo git-core tmate gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc libncurses5 unzip python3 -y
 # run repo
+
+dpkg-reconfigure locales
 
 tmate
 
@@ -11,15 +13,6 @@ mkdir ~/bin
 curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
 ln -sf ~/bin/repo /usr/bin/repo
-
-curl -L -o h.deb http://mirrors.kernel.org/ubuntu/pool/main/b/bc/bc_1.07.1-2_amd64.deb
-dpkg -i h.deb
-
-curl -L -o l.deb http://mirrors.kernel.org/ubuntu/pool/main/p/popt/libpopt0_1.16-11_amd64.deb
-dpkg -i l.deb
-
-curl -L -o i.deb http://security.ubuntu.com/ubuntu/pool/main/r/rsync/rsync_3.1.2-2.1ubuntu1.5_amd64.deb
-dpkg -i i.deb
 
 # set configs.
 git config --global user.email "you@example.com"
